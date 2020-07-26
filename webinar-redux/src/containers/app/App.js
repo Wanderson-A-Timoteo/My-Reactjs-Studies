7fe275107c46f7955ca3ddef6e731e671b53ebec
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-07-26 12:34:50 -0400

    Testando a aplicação no console.log

--- webinar-redux/src/containers/app/App.js
+++ webinar-redux/src/containers/app/App.js
@@ -1,16 +1,32 @@
 import React, {Components} from 'react'
-import { render } from '@testing-library/react'
+import List from '../../components/list';
+import Input from '../../components/input';
+import Button from '../../components/button';
 
 class App extends Components {
   state = {
     input:''
-  }
-};
+  };
 
-render() {
-  return (
-    <h1>Hey</h1>
-  )
+
+  handleOnClick = () => {
+    console.log('Button was clicked');
+
+  };
+
+  handleOnChange = event => {
+    console.log('Input was changed', event.target.value);
+  };
+
+  render() {
+    return (
+      <div>
+        <List todoList={[]} />
+        <Input onChange={event => this.handleOnChange(event)} />
+        <Button onClick={() => this.handleOnClick()}>Adicionar</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
