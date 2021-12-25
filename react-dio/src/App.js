d03a810eab18f5064527f7803413acd4615a74d4
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-12-25 11:08:01 -0400

    iniciado o desenvolvimento do componente Item

--- react-dio/src/App.js
+++ react-dio/src/App.js
@@ -1,5 +1,30 @@
 const App = () => {
-    return <h1>Hello world!</h1>
+    return (
+        <>
+            <h1>Aplicação com React</h1>
+            <ul>
+                <a
+                    href="/"
+                    className="list-group-item list-group-item-action list-group-item-dark"
+                >
+                    Item 01
+                </a>
+
+                <a
+                    href="/"
+                    className="list-group-item list-group-item-action list-group-item-dark"
+                >
+                    Item 02
+                </a>
+                <a
+                    href="/"
+                    className="list-group-item list-group-item-action list-group-item-dark"
+                >
+                    Item 03
+                </a>
+            </ul>
+        </>
+    )
 }
 
 export default App
