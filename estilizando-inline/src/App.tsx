01e6035432d5adb28bf62affadd12928008e46b5
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-04-07 15:32:17 -0400

    Adicionado exemplo de estilização inline

---
+++ estilizando-inline/src/App.tsx
@@ -0,0 +1,21 @@
+
+
+const App = () => {
+
+  return (
+    <div>      
+      <h1>Estilizando inline</h1>
+      <button 
+        style={{
+          backgroundColor: '#FF0000', 
+          color: '#FFF', 
+          border: '0'
+        }}
+      >
+        Clique aqui
+      </button>      
+    </div>
+  );
+}
+
+export default App
